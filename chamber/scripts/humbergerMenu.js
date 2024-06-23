9921214ac24f7cb01b865dd341687b3b4a0e0c35
Sergio Villarroel <35101677+nacionvirtualchile@users.noreply.github.com>
2024-06-23 04:25:09 +0200

    v40

--- chamber/scripts/humbergerMenu.js
+++ chamber/scripts/humbergerMenu.js
@@ -1,9 +1,17 @@
 const hamButton = document.querySelector('#menu');
 const navigation = document.querySelector('.navigation');
+const nav = document.querySelector('#navigation');
 
 hamButton.addEventListener('click', () => {
+    if (nav.style.backgroundColor === "#0066cc") {
+        nav.style.backgroundColor = "#ffffff";
+    } else if (nav.style.backgroundColor === "#ffffff") {
+        nav.style.backgroundColor = "#333";
+    }
     navigation.classList.toggle('open');
     hamButton.classList.toggle('open');
+
+
 });
 
 const navigationID = document.querySelector('#navigation');
