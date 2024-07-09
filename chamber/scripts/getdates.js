3c45acdd9f5d497fdd5556c22f811461ab25c89a
Sergio Villarroel <35101677+nacionvirtualchile@users.noreply.github.com>
2024-07-10 00:56:07 +0200

    v33

--- chamber/scripts/getdates.js
+++ chamber/scripts/getdates.js
@@ -3,3 +3,17 @@ document.getElementById('year').textContent = year;
 
 const lastModified = document.lastModified;
 document.getElementById('lastModified').textContent = "Ultima modificación: " + lastModified;
+
+
+
+document.getElementById('myForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const date = new Date();
+    document.getElementById('timestamp').textContent = date;
+
+    this.submit();
+});
+
+
+
