73cce897bd4de658c146b72ccbfddeb6810c4060
Sergio Villarroel <35101677+nacionvirtualchile@users.noreply.github.com>
2024-07-20 11:16:50 +0200

    v24

--- chamber/scripts/pageVisits.js
+++ chamber/scripts/pageVisits.js
@@ -2,6 +2,10 @@ const visitsDisplay = document.querySelector(".visits");
 let thisMoment = new Date();
 let differenceInMilliseconds = 0;
 
+let numVisits = Number(window.localStorage.getItem("numVisits-ls2")) || 0;
+numVisits++;
+localStorage.setItem("numVisits-ls2", numVisits);
+
 let timeOfVisit = window.localStorage.getItem("timeOfVisits-ls2") || thisMoment;
 if (timeOfVisit !== thisMoment) {
     timeOfVisit = new Date(window.localStorage.getItem("timeOfVisits-ls2"))
@@ -11,17 +15,17 @@ if (timeOfVisit !== thisMoment) {
     differenceInDays = Math.round(differenceInMilliseconds / millisecondsPerDay);
 
     if (differenceInDays > 1) {
-        visitsDisplay.textContent = "You last visited " + differenceInDays + " days ago";
+        visitsDisplay.textContent = "Your visits: " + numVisits + " - You last visited " + differenceInDays + " days ago";
     } if (differenceInDays == 1) {
-        visitsDisplay.textContent = "You last visited " + differenceInDays + " day ago";
+        visitsDisplay.textContent = "Your visits: " + numVisits + " - You last visited " + differenceInDays + " day ago";
     } else {
-        visitsDisplay.textContent = "Back so soon! Awesome! ";
+        visitsDisplay.textContent = "Your visits: " + numVisits + " - Back so soon! Awesome! ";
     }
 
 
 
 } else {
-    visitsDisplay.textContent = `Welcome! Let us know if you have any questions.`;
+    visitsDisplay.textContent = "Your visits: " + numVisits + " - Welcome! Let us know if you have any questions.";
 }
 localStorage.setItem("timeOfVisits-ls2", timeOfVisit);
 
